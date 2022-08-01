ee09865d3bfdae304b416330ff3668da5638f2b6
Tomas Gomez <tomigomez1212@gmail.com>
2022-08-01 18:27:28 -0300

    clase2

---
+++ leccion-2.js
@@ -0,0 +1,28 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+////////////////////////////////////////////////////////////////////
+const nombre = 'Tomás';
+
+const edad = 23;
+
+const desarrollador = false;
+
+const fechaNacimiento = new Date (1999, 6 , 10);
+console.log(fechaNacimiento);
+
+
+const libro = {
+    titulo: 'Once Anillos',
+    autor: 'Hugh Delehanty',
+    fecha: '21/5/2013'
+}
